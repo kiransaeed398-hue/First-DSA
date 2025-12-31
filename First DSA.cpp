3cc297d2ac2a5f8f953f04c6c27634ee73bc903e
Kiran Saeed <kiransaeed398@gmail.com>
2025-12-31 10:20:35 -0800

    Add files via upload

---
+++ First DSA.cpp
@@ -0,0 +1,38 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+//	       Boilerplate Code
+//	cout<<"hollo:";
+	
+//int a=2;
+//int b=5;
+
+//  cout<<"sum:"<<a+b<<endl;	
+  
+//  int a=2;
+//  int b=4;
+  
+//  cout<<"divi:"<<a*b<<endl;
+	
+//	int a=3;
+//	int b=5;
+	
+//	cout<<":"<<a%b<<endl;
+//	int a=3;
+//	int b=4;
+	
+//	cout<<"mod:"<<a/b<<endl;
+		
+//		cout<<"apan college:"<<endl;
+//		cout<<"kiran zahara:"<<endl;
+//		cout<<"kiran"<<"zahra"<<endl;
+
+// int age=25;
+// cout<<"age:"<<endl;
+//  char Grade= 'a';
+//  cout<<"Grade:"<<endl;
+//	float IP=3.41;
+//	cout<"IP:";	
+		
+}
